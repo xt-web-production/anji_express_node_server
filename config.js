db636612d0d10d58bb155d800cd2542c07c2d200
shengxx <janisesheng@cform.io>
2018-03-13 23:39:25 +0800

    no message

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ module.exports = {
     code: 1,
     //sql - server地址
     mysql: {
-        host: '118.31.19.0', //118.31.19.0  106.14.144.162 localhost
+        host: '106.14.144.162', //118.31.19.0  106.14.144.162 localhost
         user: 'root',
-        password: 'sXX12345678', // 12345678
+        password: '12345678', // 12345678 sXX12345678
         database: 'sxx',
         port: 3306,
     },
